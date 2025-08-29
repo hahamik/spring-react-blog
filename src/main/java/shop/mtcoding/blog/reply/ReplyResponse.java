7df3fe126cf4ad48718e8ccae4831d87140b6be3
hahamik <dlapdl0615@gmail.com>
2025-08-29 09:49:55 +0900

    댓글 쓰기 수정

--- src/main/java/shop/mtcoding/blog/reply/ReplyResponse.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyResponse.java
@@ -3,16 +3,26 @@ package shop.mtcoding.blog.reply;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 public class ReplyResponse {
 
     @Data
-    public static class SaveDTO {
+    public static class DTO {
         private Integer boardId;
+        private Integer replyId;
+        private Integer userId;
+        private Boolean isReplyOwner;
         private String comment;
+        private Timestamp createdAt;
 
-        public SaveDTO(Integer boardId, Reply reply) {
-            this.boardId = boardId;
+        public DTO(Reply reply) {
+            this.boardId = reply.getBoard().getId();
+            this.replyId = reply.getId();
+            this.userId = reply.getUser().getId();
+            this.isReplyOwner = reply.isReplyOwner();
             this.comment = reply.getComment();
+            this.createdAt = reply.getCreatedAt();
         }
     }
 }
